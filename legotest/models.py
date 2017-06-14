ec697f3c1aedc20ad4163577d05514b052b08d57
Tlezip <title0088@gmail.com>
2017-06-14 15:55:20 +0700

    Modify Model of lego-test,question

--- legotest/models.py
+++ legotest/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 
-# Create your models here.
 class Test(models.Model): 
-	# sectionID = models.ForeignKey('Section')
 	name = models.CharField(max_length=50) #Test Name
-	Img_name = models.ImageField(upload_to = 'image/')
+	image = models.ImageField(upload_to = 'image/')
 	typetest = models.CharField(null = True , max_length=50)
 	category = models.CharField(max_length=50)
 	overview = models.TextField()
@@ -16,18 +14,35 @@ class Test(models.Model):
 	descripe = models.TextField()
 
 class Section(models.Model):
-	testid = models.ForeignKey('Test' , default = 0)
-	sectionNum = models.AutoField(primary_key=True)
-	fullscore = models.IntegerField(default = 0)
+	PULL_CHOICES = (
+		(0, 'Basic'),
+		(1, 'Bank')
+	)
+
+	test = models.ForeignKey('Test', default = 0)
+	pull = models.IntegerField(choices=PULL_CHOICES)
+	num = models.AutoField(primary_key=True)
+	name = models.CharField(max_length=50, default='')
+	full_score = models.IntegerField(default=0)
 	pass_score = models.IntegerField()
+
 	# SectionID = models.AutoField(primary_key=True)
 	# typequestion = models.CharField(max_length=50)
 
-# class Question(models.Model):
-
+class Question(models.Model):
+    questionid = models.IntegerField(null=True)
 	# sectionNum = models.ForeignKey(Section)
 	# typequestion = models.CharField(default = "Section.Type << how to do" , max_length=50)
 	# name = models.CharField(max_length=50)
 	# answer = models.CharField(max_length=50)
 	# numshow = models.IntegerField()
 
+# class Condition(models.Model):
+# 	CONDITION_CHOICE = (
+#         (0, 'random'),
+#         (1, 'choose')
+# 	)
+# 	section = models.ForeignKey('Section')
+# 	bank = models.ForeignKey('legotestquestion.Bank')
+# 	typecondition = models.IntegerField(choices=CONDITION_CHOICE)
+# 	pass
--- legotestquestion/models.py
+++ legotestquestion/models.py
@@ -1,24 +1,24 @@
 from django.db import models
 
-# Create your models here.
-class Question(models.Model):
-	QUESTION_TYPE_CHOICES = (
-    (0, 'Multiple Choice'),
-    (1, 'Fill Description'),
-    (2, 'Fill the Box'),
-    (3, 'Match'),
-	)
-	question_text = models.TextField()
-	question_type = models.IntegerField(choices=QUESTION_TYPE_CHOICES , default=0)
-	question_answer = models.CharField(max_length=50)
-	question_num_show = models.IntegerField()
-	question_isrequire = models.IntegerField(default = 0)
-	pass
 
 class Bank(models.Model):
-	questions = models.ManyToManyField(Question)
+	# questions = models.ManyToManyField('legotestquestion.Question')
+	name = models.CharField(max_length=50)
 
-
+class Question(models.Model):
+	TYPE_CHOICES = (
+        (0, 'Multiple Choice'),
+        (1, 'Fill Description'),
+        (2, 'Fill the Box'),
+        (3, 'Match'),
+	)
+	bank = models.ForeignKey('Bank',null=True)
+	text = models.TextField()
+	type = models.IntegerField(choices=TYPE_CHOICES, default=0)
+	answer = models.CharField(max_length=50)
+	num_show = models.IntegerField()
+	is_require = models.IntegerField(default=0)
+	
 
 class Choice(models.Model):
 	questionid = models.ForeignKey('Question')
