93565798a99e4b39109f60652ca6384e22b733f8
Tlezip <title0088@gmail.com>
2017-06-14 11:47:53 +0700

    Add model legotest,legoquestiontest

--- legotestquestion/models.py
+++ legotestquestion/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Bank(models.Model):
+	questions = models.ManyToManyField(Question)
+
+class Question(models.Model):
+	question_text = models.TextField()
+	question_type = models.IntegerField(choices=QUESTION_TYPE_CHOICES)
+	question_answer = models.CharField(max_length=50)
+	question_num_show = models.IntegerField()
+	question_isrequire = models.IntegerField(default = 0)
+	QUESTION_TYPE_CHOICES = (
+    (0, 'Multiple Choice'),
+    (1, 'Fill Description'),
+    (2, 'Fill the Box'),
+    (3, 'Match'),
+	)
+
+class Choice(models.Model):
+	questionid = models.ForeignKey('Question')
+	choice_text = models.CharField(max_length=50)
