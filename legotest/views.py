4a2d818c51969914608a0ba04afbba0f7198f667
Tlezip <title0088@gmail.com>
2017-06-19 18:09:08 +0700

    complete is_display

--- legotest/views.py
+++ legotest/views.py
@@ -7,29 +7,6 @@ from legotest.models import Test
 from legotest.serializer import TestSerializer
 from rest_framework import viewsets
 
-# Create your views here.
-# @csrf_exempt
-# def request_detail(request):
-#     """
-#     Retrieve, update or delete a code snippet.
-#     """
-#     # try:
-#     #     snippet = Snippet.objects.get(pk=pk)
-#     # except Snippet.DoesNotExist:
-#     #     return HttpResponse(status=404)
-
-#     if request.method == 'GET':
-#         return HttpResponse(status=200)
-
-#     elif request.method == 'PUT':
-#         return HttpResponse(status=404)
-
-#     elif request.method == 'POST':
-#         return HttpResponse(status=404)
-
-#     elif request.method == 'PATCH':
-#         return HttpResponse(status=404)
-
 class TestViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Test.objects.all()
+    queryset = Test.objects.filter(is_display=True)
     serializer_class = TestSerializer
